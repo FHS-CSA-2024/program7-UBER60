1954335c86aa40bae7f47cb2e285e97f027a2162
UBER60 <rcho5318@gmail.com>
2024-08-26 14:37:53 -0700

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,7 +1,29 @@
 //import stuff here
-
+import java.util.Scanner;
 //Your code here
-
+public class Program3{
+  public static void main(String[] args){
+    int sb;
+    int k;
+    int sn;
+    Scanner x = new Scanner(System.in);
+    System.out.print("Enter schrute-bucks: ");
+    sb = x.nextInt();
+    System.out.println();
+    System.out.print("Enter klevins: ");
+    k = x.nextInt();
+    System.out.println();
+    System.out.print("Enter stanley-nickels: ");
+    sn = x.nextInt();
+    System.out.println();
+    double k1 = k / 20.0;
+    double sn1 = sn / 240.0;
+    double total = sn1 + k1 + sb;
+    double total1 = Math.round(total * 100.0) / 100.0;
+    System.out.print("Decimal schrute-bucks = $");
+    System.out.println(total1);
+  }
+}
 //Paste console output below:
 /*
 
