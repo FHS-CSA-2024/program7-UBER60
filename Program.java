6fb28f08171f9efb2ae377a64c4473709a00ac5d
UBER60 <rcho5318@gmail.com>
2024-08-28 11:46:56 -0700

    Update Program.java

--- Program.java
+++ Program.java
@@ -6,6 +6,7 @@ public class Program{
     int sb;
     int k;
     int sn;
+    final double twenty = 20.0;
     Scanner x = new Scanner(System.in);
     System.out.print("Enter schrute-bucks: ");
     sb = x.nextInt();
@@ -16,7 +17,7 @@ public class Program{
     System.out.print("Enter stanley-nickels: ");
     sn = x.nextInt();
     System.out.println();
-    double k1 = k / 20.0;
+    double k1 = k / twenty;
     double sn1 = sn / 240.0;
     double total = sn1 + k1 + sb;
     double total1 = Math.round(total * 100.0) / 100.0;
